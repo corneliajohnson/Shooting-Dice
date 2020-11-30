e2fe2854a6e7170efd6f0adfc3b11aa21528c7ae
corneliajohnson <corneliaj91@gmail.com>
2020-11-30 13:07:02 -0500

    trash talking player

--- SmackTalkingPlayer.cs
+++ SmackTalkingPlayer.cs
@@ -1,10 +1,21 @@
+using System;
+using System.Collections.Generic;
+
 namespace ShootingDice
 {
-    // TODO: Complete this class
+  // TODO: Complete this class
 
-    // A Player who shouts a taunt every time they roll dice
-    public class SmackTalkingPlayer
+  // A Player who shouts a taunt every time they roll dice
+  public class SmackTalkingPlayer : Player
+  {
+    public void NewTaunt()
     {
-        public string Taunt { get; }
+      List<string> tauntStrings = new List<string>(){
+            "Here come the boom!", "You aint got nothing on me!", "If your not first you're last!", "I'm like that!"
+        };
+      Random randomTaunt = new Random();
+      int index = randomTaunt.Next(tauntStrings.Count);
+      Console.WriteLine($"{Name}:  {tauntStrings[index]}");
     }
+  }
 }
