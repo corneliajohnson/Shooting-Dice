76df340e20210822191257446616018f720e926a
corneliajohnson <corneliaj91@gmail.com>
2020-11-30 15:49:03 -0500

    Human Player inheritance

--- HumanPlayer.cs
+++ HumanPlayer.cs
@@ -1,10 +1,37 @@
+using System;
+
 namespace ShootingDice
 {
-    // TODO: Complete this class
+  // TODO: Complete this class
 
-    // A player the prompts the user to enter a number for a roll
-    public class HumanPlayer
+  // A player the prompts the user to enter a number for a roll
+  public class HumanPlayer : Player
+  {
+    public override void Play(Player other)
     {
+      // Call roll for "this" object and for the "other" object
+      Console.Write($"Enter a nember between 1 and {DiceSize}:   ");
 
+      int myRoll = int.Parse(Console.ReadLine());
+
+      int otherRoll = other.Roll();
+
+      Console.WriteLine($"{Name} rolls a {myRoll}");
+      Console.WriteLine($"{other.Name} rolls a {otherRoll}");
+      if (myRoll > otherRoll)
+      {
+        Console.WriteLine($"{Name} Wins!");
+      }
+      else if (myRoll < otherRoll)
+      {
+        Console.WriteLine($"{other.Name} Wins!");
+      }
+      else
+      {
+        // if the rolls are equal it's a tie
+        Console.WriteLine("It's a tie");
+      }
     }
+
+  }
 }
